94ac97fc7e20a2fb0e5354db463830b1182c6f1c
Nusrat <nusratilla.88@gmail.com>
2024-05-01 00:33:21 +0500

    2

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>Hello VaBuChi</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Aperiam dolor quaerat doloremque voluptatem distinctio qui
+          facere reprehenderit culpa possimus unde.</p>
+        <button className='btn'>Ma'lumot</button>
+      </div>
+    </div>
+  )
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/vabuchiW.png'
 
 function Navbar() {
   return (
     <nav>
-
+      <div className="navBar container">
+        <img src={logo} alt="" className='logo'/>
+        <ul>
+          <li>Bosh sahifa</li>
+          <li>Hizmatlar</li>
+          <li>Gallireya</li>
+          <li><button className='btn'>Aloqa</button></li>
+        </ul>
+      </div>
 
     </nav>
   )
